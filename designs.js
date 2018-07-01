b7019b955b8a2e0d6c6d21cff93b06cd9c6998c0
didymus707 <38667458+didymus707@users.noreply.github.com>
2018-07-02 00:07:28 +0100

    updated work

--- designs.js
+++ designs.js
@@ -1,10 +1,40 @@
 // Select color input
 // Select size input
+var color, height, width;
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+$("#sizePicker").submit(function (event) {
+  event.preventDefault();
+  height = $("#inputHeight").val();
+  width = $("#inputWidth").val();
+  makeGrid(height, width);
+});
+
+function makeGrid(height, width) {
+  $("tr").remove();
+  //removes all everything in the grid when called
 
 // Your code goes here!
+for (var x=1; x<=height; x++) {
+  // $("#pixelCanvas").append("<tr id=table>" + x + "</tr>");
+  // for (var y=1; y<=width; y++) {
+  //   $("#table"+ x).append("<td></td>");
+  //   }
+  $("#pixelCanvas").append("<tr id=table" + x + "></tr>");
+ for (var y=1; y<=width; y++) {
+  $("#table"+ x).append("<td></td>");
+  }
+  }
+  $("td").click(function addColor() {
+    color = $("#colorPicker").val();
 
+    if ($(this).attr("style")) {
+      $(this).removeAttr("style");
+// removes every box clicked 
+
+    } else {
+      $(this).attr("style", "background-color:" + color);
+    }
+  });
 }
